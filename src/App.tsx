671b5fc68bf0f1e032f3c4b2d75b1044a696071a
Shawn Long <shawnlong@Shawns-MacBook-Pro.local>
2022-12-04 15:41:45 -0800

    Added basic componenets and associated styles for tic tac toe

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.scss';
-
-function App() {
-  return <div className="App">Hey there</div>;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Game} from './components/Game';
 import './index.scss';
-import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<Game />);
+}
