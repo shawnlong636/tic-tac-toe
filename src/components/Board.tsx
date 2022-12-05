3b935aef1ffff53982be8190fdc4f03778723112
Shawn Long <shawnlong636@gmail.com>
2022-12-04 16:47:00 -0800

    moved state from square to board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,9 +2,27 @@ import React from 'react';
 import {Square} from './Square';
 import './Board.scss';
 
-export class Board extends React.Component {
-  renderSquare(i: Number) {
-    return <Square value={i} />;
+interface BoardState {
+  squares: Array<string>;
+}
+export class Board extends React.Component<{}, BoardState> {
+  state: BoardState = {
+    squares: Array(9).fill(''),
+  };
+
+  renderSquare(i: number) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  handleClick(i: number) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   render() {
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Square.scss';
 
-export class Square extends React.Component<{value: Number}, {value: String}> {
-  state: {value: String} = {
-    value: '',
-  };
+interface SquareProps {
+  value: string;
+  onClick(): void;
+}
+
+export class Square extends React.Component<SquareProps, {}> {
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
